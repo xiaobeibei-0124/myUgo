4059a25956d4ce87df3a85d0f53af322638166eb
123456 <1642229324@qq.com>
2020-04-10 20:24:41 +0800

    搜索与轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,6 +1,36 @@
 <template>
 	<view>
-		首页
+		<!-- 搜索区域 -->
+		<view class="search">
+			<view class="search-input">
+				<input type="text" placeholder="请输入搜索商品">
+			</view>
+			<view class="search-content"></view>
+		</view>
+		<!-- 轮播图区域 -->
+		<view class="swiper-box">
+			<swiper autoplay 
+							indicator-dots 
+							indicator-color="rgba(255,255,255,.5)" 
+							indicator-active-color="#fff">
+				<swiper-item>
+					<navigator url="">
+						<image src="../../static/uploads/banner1.png"></image>
+					</navigator>
+				</swiper-item>
+				<swiper-item>
+					<navigator url="">
+						<image src="../../static/uploads/banner2.png"></image>
+					</navigator>
+				</swiper-item>
+				<swiper-item>
+					<navigator url="">
+						<image src="../../static/uploads/banner3.png"></image>
+					</navigator>
+				</swiper-item>
+			</swiper>
+		</view>
+		
 	</view>
 </template>
 
@@ -20,20 +50,33 @@
 	}
 </script>
 
-<style>
-	.content {
-		text-align: center;
-		height: 400upx;
+<style lang='less' scoped>
+	.search {
+		.search-input {
+			background-color: #eb4450;
+			padding: 20rpx 15rpx;
+			input {
+				background-color: #fff;
+				color: #ccc;
+				height: 60rpx;
+				padding-left: 10rpx;
+				font-size: 28rpx;
+				border-radius: 5rpx;
+			}
+		}
 	}
-
-	.logo {
-		height: 200upx;
-		width: 200upx;
-		margin-top: 200upx;
-	}
-
-	.title {
-		font-size: 36upx;
-		color: #8f8f94;
+	.swiper-box {
+		swiper {
+			width: 750rpx;
+			height: 340rpx;
+			navigator {
+				width: 750rpx;
+				height: 340rpx;
+				image {
+				width: 100%;
+				height: 100%;
+			}
+			}	
+		}
 	}
 </style>
